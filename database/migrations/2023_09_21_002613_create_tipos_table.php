1d2f6f57fb1db6c99a25d8ab11e4f7268b459ab4
MuriloHcorrea <murilo.honorato.correa@gmail.com>
2023-09-22 19:45:56 -0300

    migrate.adocao

--- database/migrations/2023_09_21_002613_create_tipos_table.php
+++ database/migrations/2023_09_21_002613_create_tipos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tipos', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_tipo');
             $table->timestamps();
         });
     }
