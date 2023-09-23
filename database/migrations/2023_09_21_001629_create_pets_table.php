ade80071c6881b3266240e48fbbb1570f3ca907a
MuriloHcorrea <murilo.honorato.correa@gmail.com>
2023-09-22 21:32:23 -0300

    finalizando.migration

--- database/migrations/2023_09_21_001629_create_pets_table.php
+++ database/migrations/2023_09_21_001629_create_pets_table.php
@@ -12,8 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pets', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_pet');
+            $table->increments('id_raca');
+            $table->increments('id_porte');
+            $table->increments('id_cor');
+            $table->increments('id_sexo');
+            $table->increments('id_historico');
+            $table->increments('id_user');
+            $table->string('nome',45);
+            $table->date('dt_nascimento');
+            $table->text('deficiencia');
+            $table->string('castrado',45);
+            $table->string('peso',45);
+            $table->text('vacina');
             $table->timestamps();
+            $table->softDeletes();
+
+
         });
     }
 
